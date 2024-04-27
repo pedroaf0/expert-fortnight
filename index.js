46cf1af6bb28bb53b606839fd37a778c0cca3c30
Pedro Afonso Machado Nunes <54213349+pedroaf0@users.noreply.github.com>
2024-04-27 11:09:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ client.on("messageCreate", async (message) => {
   });
 
 
-client.login("dm_me_chatbot", "33551047pP!")
+client.login("dm_me_bot2", "33551047pP!")
 
   //abre um servidor http para manter o bot rodando
   const http = require('http');
